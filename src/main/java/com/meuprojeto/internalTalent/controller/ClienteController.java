dc140f8c762aa83e2243bc370941529d92fa2727
adolfobjj <luizadolfo7@hotmail.com>
2023-09-13 10:36:01 -0300

    Criação do metodo Post e Delete

--- src/main/java/com/meuprojeto/internalTalent/controller/ClienteController.java
+++ src/main/java/com/meuprojeto/internalTalent/controller/ClienteController.java
@@ -4,7 +4,7 @@ import com.meuprojeto.internalTalent.entity.Cliente;
 import com.meuprojeto.internalTalent.repository.Clientes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -18,11 +18,31 @@ private Clientes clientes;
 
     @GetMapping("/api/clientes/{id}")
     @ResponseBody
-    public ResponseEntity getClienteById(@PathVariable Integer id ){
+    public ResponseEntity getClienteById( @PathVariable Integer id ){
         Optional<Cliente> cliente = clientes.findById(id);
 
         if(cliente.isPresent()){
             return ResponseEntity.ok( cliente.get() );
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/api/clientes")
+    @ResponseBody
+    public ResponseEntity save( @RequestBody Cliente cliente ){
+        Cliente clienteSalvo = clientes.save(cliente);
+        return ResponseEntity.ok(clienteSalvo);
+    }
+
+    @DeleteMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity delete( @PathVariable Integer id ){
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if(cliente.isPresent()){
+            clientes.delete( cliente.get() );
+            return ResponseEntity.noContent().build();
         }
 
         return ResponseEntity.notFound().build();
--- src/main/java/com/meuprojeto/internalTalent/repository/Clientes.java
+++ src/main/java/com/meuprojeto/internalTalent/repository/Clientes.java
@@ -11,10 +11,18 @@ import java.util.List;
 public interface Clientes extends JpaRepository<Cliente, Integer > {
 
 
-    List<Cliente> encontrarPorNome(  String nome );
+    @Query(value = " select * from cliente c where c.nome like '%:nome%' ", nativeQuery = true)
+    List<Cliente> encontrarPorNome( @Param("nome") String nome );
+
+    @Query(" delete from Cliente c where c.nome =:nome ")
+    @Modifying
     void deleteByNome(String nome);
+
     boolean existsByNome(String nome);
-    Cliente findClienteFetchPedidos(  Integer id );
+
+    @Query(" select c from Cliente c left join fetch c.pedidos where c.id = :id  ")
+    Cliente findClienteFetchPedidos( @Param("id") Integer id );
+
 
 
 }
